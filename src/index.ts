8628d7bee146286258e3fca38a59bb6e47835983
Diyorbek Rustamjonov <bdvhattab@gmail.com>
2024-01-01 01:56:46 +0500

    feat: added example NovaPool class implementations

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello world');
+export interface NovaPoolOptions {
+  filename: string | null;
+  minThread: number | null;
+  minThreads?: number;
+  maxThreads?: number;
+  idleTimeout?: number;
+  maxQueue?: number | 'auto';
+}
+
+export class NovaPool {
+  constructor(private readonly options: NovaPoolOptions) {
+    if (!this.options.filename) {
+      throw new Error('FileName not found');
+    }
+  }
+}
