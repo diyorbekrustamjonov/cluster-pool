8fbb784a3ab8cd9540a47fae7bf25979520a7617
Diyorbek Rustamjonov <bdvhattab@gmail.com>
2024-01-02 21:31:08 +0500

    feat: added worker new symbols

--- src/common/symbols.ts
+++ src/common/symbols.ts
@@ -1,5 +1,9 @@
 export namespace Symbols {
   export namespace Task {
     export const kQueueOptions = Symbol.for('NovaPool.queueOptions');
+    export const kTask = Symbol.for('NovaPool.task');
+    export const kValue = Symbol.for('NovaPool.valueOf');
+    export const kMovable = Symbol.for('NovaPool.kMovable');
+    export const kTransferable = Symbol.for('NovaPool.transferable');
   }
 }
