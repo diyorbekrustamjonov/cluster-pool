7629480aaebc5a12ffb9e1ee7ec8fd48d7ecc45c
Diyorbek Rustamjonov <bdvhattab@gmail.com>
2024-01-03 21:06:06 +0500

    style: removed comments

--- src/interfaces/pool/channel.ts
+++ src/interfaces/pool/channel.ts
@@ -1,7 +1,4 @@
 export interface NovaPoolChannel {
-  // Workers subscribe to this event
   onMessage(callback: (message: any) => void): void;
-
-  // Called with worker's messages
   postMessage(message: any): void;
 }
